832d52f8a249ad5fc85ebb861fec068a29bb4746
chrislaf63 <125891353+chrislaf63@users.noreply.github.com>
2024-09-06 16:56:46 +0200

    Ajout du charts et style

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {Chart as ChartJS} from 'chart.js/auto';
+import {Bar} from "react-chartjs-2";
 import {Transaction} from "./Transaction";
 
 function MainPage() {
@@ -58,7 +60,7 @@ function MainPage() {
 
             console.log('Response status:', response.status);
 
-            if (response.status === 200 || response.status=== 201 || response.status === 204) {
+            if (response.status === 200 || response.status === 201 || response.status === 204) {
                 console.log('Transaction deleted successfully');
                 setData([...data, newTransaction]);
             } else {
@@ -71,65 +73,88 @@ function MainPage() {
 
     }
 
-        const onDelete = async (transactionId) => {
-            const token = localStorage.getItem('authToken'); // Récupérez le token depuis le localStorage
+    const onDelete = async (transactionId) => {
+        const token = localStorage.getItem('authToken'); // Récupérez le token depuis le localStorage
 
-            try {
-                const response = await axios.delete(`http://localhost:8000/api/transactions/${transactionId}`, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}` // Ajoutez le token aux en-têtes
-                    },
-                });
+        try {
+            const response = await axios.delete(`http://localhost:8000/api/transactions/${transactionId}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}` // Ajoutez le token aux en-têtes
+                },
+            });
 
-                console.log('Response status:', response.status);
+            console.log('Response status:', response.status);
 
-                if (response.status === 200 || response.status === 201 || response.status === 204) {
-                    console.log('Transaction deleted successfully');
-                    setData((curr) => curr.filter((transaction) => transaction.id !== transactionId));
-                } else {
-                    alert('Une erreur est survenue lors de la suppression');
-                }
-            } catch (error) {
-                console.error('Erreur :', error);
+            if (response.status === 200 || response.status === 201 || response.status === 204) {
+                console.log('Transaction deleted successfully');
+                setData((curr) => curr.filter((transaction) => transaction.id !== transactionId));
+            } else {
                 alert('Une erreur est survenue lors de la suppression');
             }
-        };
+        } catch (error) {
+            console.error('Erreur :', error);
+            alert('Une erreur est survenue lors de la suppression');
+        }
+    };
 
 
-        const TransactionsList = data.map((transaction) => {
-            return (
-                <Transaction id={transaction.id}
-                             name={transaction.name}
-                             type={transaction.type}
-                             amount={transaction.amount}
-                             user={transaction.user}
-                             onDelete={(id) => {
-                                 onDelete(id)
-                             }}
-                />
-            );
-        });
-        console.log(TransactionsList);
+    const TransactionsList = data.map((transaction) => {
         return (
-            <div className="min-h-screen bg-gray-300 flex flex-col items-center">
-                <header className="w-full bg-[#8c4a08] p-6 text-center text-black font-bold text-3xl">
-                    OptiGest
-                </header>
-                {user && <h1>Bonjour {user.name}</h1>}
-                <form  onSubmit={handleSubmit} className="flex flex-col w-60 gap-y-1.5">
-                    <input type="text" name="name" placeholder="Nom de l'opération"/>
-                    <select name="type">
-                    <option value="credit" selected>Crédit</option>
-                    <option value="debit">Débit</option>
-                    </select>
-                    <input type="text" name="amount" placeholder="Montant"/>
-                    <input className="bg-green-300" type="submit" value="Ajouter"/>
-                </form>
-                <div>{TransactionsList}</div>
-                {user && <p>Solde : {user.balance}</p>}
-            </div>
+            <Transaction id={transaction.id}
+                         name={transaction.name}
+                         type={transaction.type}
+                         amount={transaction.amount}
+                         user={transaction.user}
+                         onDelete={(id) => {
+                             onDelete(id)
+                         }}
+            />
         );
-    }
+    });
+    console.log(TransactionsList);
+    return (
+        <div className="min-h-screen bg-gray-300 flex flex-col items-center">
+            <header className="w-full bg-[#8c4a08] p-6 text-center text-black font-bold text-3xl">
+                OptiGest
+            </header>
+            {user && <h2 className="font-bold text-xl my-6">Bonjour {user.name}</h2>}
+            <div className="flex justify-center gap-5">
+                <div>{TransactionsList}</div>
+                <div className="bg-gray-400/40 p-4 rounded-lg">
+                    <h3 className="text-center font-semibold mb-4">Ajouter une opération</h3>
+                    <form onSubmit={handleSubmit} className="flex flex-col w-60 gap-y-3">
+                        <input className="py-1 px-2 rounded-md" type="text" name="name"
+                               placeholder="Nom de l'opération"/>
+                        <select className="py-1 px-2 rounded-md" name="type">
+                            <option value="credit" selected>Crédit</option>
+                            <option value="debit">Débit</option>
+                        </select>
+                        <input className="py-1 px-2 rounded-md" type="text" name="amount" placeholder="Montant"/>
+                        <input className="bg-green-300 py-1 px-2 rounded-md hover:cursor-pointer hover:bg-green-100"
+                               type="submit" value="Ajouter"/>
+                    </form>
+                </div>
 
-            export default MainPage;
+            </div>
+
+            <div>{user && <p className="text-xl font-semibold bg-gray-200 px-20 mb-5">Solde : {user.balance} €</p>}</div>
+            <div className="bg-gray-200">
+                    <Bar data={{
+                        labels: data.map((transaction) => transaction.name),
+                        datasets: [
+                            {
+                                label: 'Montant',
+                                data: data.map((transaction) => transaction.amount),
+                                backgroundColor: data.map((transaction) => transaction.type === 'credit' ? 'green' : 'red'),
+                            },
+                        ],
+                    }}/>
+                </div>
+
+
+        </div>
+    );
+}
+
+export default MainPage;
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,25 +1,32 @@
 import React from "react";
 
-export function Transaction({ id, name, type, amount, user, user_id, onDelete }) {
+export function Transaction({id, name, type, amount, user, user_id, onDelete}) {
     return (
-        <div className="flex">
-            <div className="bg-gray-200 p-4 my-2 text-center">
-                <p>N° Opération</p>
+        <div className="flex h-24 ">
+            <div className="bg-gray-200 p-4 my-2 text-center w-20">
+                <p>N° Op.</p>
                 <p>{id}</p>
             </div>
-            <div className="bg-gray-200 p-4 my-2 text-center">
+            <div className="bg-gray-200 p-4 my-2 text-center w-48">
                 <p>Opération:</p>
                 <p>{name}</p>
             </div>
-            <div className="bg-gray-200 p-4 my-2 text-center">
+            <div className="bg-gray-200 p-4 my-2 text-center w-52">
                 <p>Type d'opération:</p>
                 <p>{type}</p>
             </div>
-            <div className="bg-gray-200 p-4 my-2 text-center">
+            <div className="bg-gray-200 p-4 my-2 text-center w-32">
                 <p>Montant:</p>
-                <p>{amount}</p>
+                <p>{amount} €</p>
             </div>
-            <button onClick={() => onDelete(id)}>❌</button>
+            <div className="bg-gray-200 my-2">
+                <button className="bg-gray-200 mt-3" onClick={() => console.log("modify")}>
+                    🖊️
+                </button>
+            </div>
+            <div className="bg-gray-200 my-2">
+                <button className="bg-gray-200 mt-3" onClick={() => onDelete(id)}>❌</button>
+            </div>
         </div>
     );
 }
